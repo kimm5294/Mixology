48ef222ecdcb977f49aabca4600bed059cc77a7a
Matthew Kim <kimm5294@gmail.com>
2017-05-25 18:27:12 -0400

    add associations to drink model

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -2,4 +2,6 @@ class Drink < ApplicationRecord
   validates :name, presence: true
   validates :api_id, presence: true, uniqueness: true
 
+  has_many :drinks_users
+  has_many :users, through: :drinks_users
 end
