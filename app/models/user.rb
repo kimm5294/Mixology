4d36ff4ca719b2d09b79ccca0d9e3f9de9e37aa3
Matthew Kim <kimm5294@gmail.com>
2017-05-25 17:23:05 -0400

    add validations for password and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  validates :first_name
-  validates :last_name
-  validates :email
-  validates :password
+  validates :first_name, :last_name, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, presence: true, length: {minimum: 6}
 
   has_secure_password
 
