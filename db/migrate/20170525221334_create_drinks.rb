68ca548be273f0e2cb0b1d4e68182aca04aa5d66
Matthew Kim <kimm5294@gmail.com>
2017-05-25 18:15:03 -0400

    create migration for drinks class model

--- db/migrate/20170525221334_create_drinks.rb
+++ db/migrate/20170525221334_create_drinks.rb
@@ -1,8 +1,10 @@
 class CreateDrinks < ActiveRecord::Migration[5.1]
   def change
     create_table :drinks do |t|
+      t.string :name, null: false
+      t.string :api_id, null: false
 
-      t.timestamps
+      t.timestamps(null: false)
     end
   end
 end
