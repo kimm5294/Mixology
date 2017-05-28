d3cbd6ff14ea846d9fd264c31dba513760c400e0
Matt Kim <kimm5294@gmail.com>
2017-05-27 21:07:23 -0400

    create routes for drinks_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :users
+resources :users, except: [:index]
 
 resources :drinks
+
+resources :drinks_users, only: [:create, :destroy]
 
 resources :sessions, only: [:destroy, :new]
 
