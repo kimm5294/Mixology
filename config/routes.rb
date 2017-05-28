6388b15fc001810074a83de1df5ee15ec476c890
Matt Kim <kimm5294@gmail.com>
2017-05-27 21:22:49 -0400

    add routes for searching through drinks

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 
 resources :users, except: [:index]
 
-resources :drinks, only: [:create, :show]
+resources :drinks, only: [:create, :show] do
+  collection do
+    get 'search'
+    post 'search_post'
+  end
+end
 
 resources :drinks_users, only: [:create, :destroy]
 
