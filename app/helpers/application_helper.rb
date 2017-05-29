2a06df81bbeb4f9d2c8107c66d0bf400814c36f6
Matt Kim <kimm5294@gmail.com>
2017-05-28 22:08:45 -0400

    create require login method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,13 @@ module ApplicationHelper
     false
   end
 
+  def require_login
+    if !logged_in?
+      flash[:error] = "This page requires a login"
+      redirect_to '/sessions/new'
+    end
+  end
+
   def current_user
     User.find_by_id(session[:current_user_id])
   end
