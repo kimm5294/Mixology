22ef33c42720bea1dcca0a711de386c9fa7c0542
Matt Kim <kimm5294@gmail.com>
2017-05-28 22:04:28 -0400

    create helper methods current user and logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def logged_in?
+    return true if session[:current_user_id]
+    false
+  end
+
+  def current_user
+    User.find_by_id(session[:current_user_id])
+  end
 end
