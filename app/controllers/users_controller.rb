f7f2e0a3ea4d722aa0464d535d3400643b0b37c5
Matt Kim <kimm5294@gmail.com>
2017-05-28 22:04:10 -0400

    Create routes for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:current_user_id] = @user.id
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find_by(id: current_user.id) if logged_in?
+  end
+
 end
