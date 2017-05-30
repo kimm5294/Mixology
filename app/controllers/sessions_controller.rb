b759594ce78248577de8fbf2397577c9825945b8
Matt Kim <kimm5294@gmail.com>
2017-05-29 22:29:19 -0400

    create route for sessions create and new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,25 @@
 class SessionsController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user
+      if @user.authenticate(params[:password])
+        session[:current_user_id] = @user.id
+        redirect_to @user
+      else
+        @errors = ["Email and password combination do not match our records"]
+        render "new"
+      end
+    else
+      @errors = ["Email does not match our records"]
+      render "new"
+    end
+  end
+
   def destroy
     session.clear
     redirect_to '/'
