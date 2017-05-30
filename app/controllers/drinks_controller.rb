370611dbcecdeef776a57573ad3f6428dcee0fc0
Matt Kim <kimm5294@gmail.com>
2017-05-30 11:24:51 -0400

    include drinks helper module and create routing methods for index

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,15 @@
 class DrinksController < ApplicationController
+  include DrinksHelper
+
+  def index
+    @popular_drinks = most_popular_drinks
+  end
+
+  def show
+    @drink = Drink.find_by(id: params[:id])
+  end
+
+  def create
+
+  end
 end
