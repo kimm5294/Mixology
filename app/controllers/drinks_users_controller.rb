422b2612c1d7562fccd33114cbcf44fa6a346fed
Matt Kim <kimm5294@gmail.com>
2017-05-31 10:13:09 -0400

    create drinks users controller

---
+++ app/controllers/drinks_users_controller.rb
@@ -0,0 +1,11 @@
+class DrinksUsersController < ApplicationController
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ spec/controllers/drinks_users_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DrinksUsersController, type: :controller do
+
+end
