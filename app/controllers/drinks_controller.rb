f87ad59c21e64842955909ff12ceb6b61c1e6c9f
Matt Kim <kimm5294@gmail.com>
2017-05-30 21:02:43 -0400

    create display route in controller

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,5 +1,6 @@
 class DrinksController < ApplicationController
   include DrinksHelper
+  include AbsolutApiHelper
 
   def index
     @popular_drinks = most_popular_drinks
@@ -9,7 +10,16 @@ class DrinksController < ApplicationController
     @drink = Drink.find_by(id: params[:id])
   end
 
+  def search
+    @results = search_name(params[:search])
+  end
+
   def create
 
   end
+
+  def display
+    @drink = find_drink(params[:api])
+  end
+
 end
